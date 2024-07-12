5cb5232f1ede460ed8c0c962636135076a8db5e9
pyongheng <107157547+pyongheng@users.noreply.github.com>
2024-07-12 17:06:27 +0900

    Add files via upload

---
+++ planktoscope/segmentation.sh
@@ -0,0 +1,31 @@
+# activate enviroment
+conda deactivate
+conda deactivate
+source /home/ecbsu/Desktop/seg_dir/seg_dir/bin/activate
+
+### Input parameters
+outdir='/media/ecbsu/vEM_data/planktoscope_data'
+
+# To run all dates
+for date in /media/ecbsu/rootfs/home/pi/data/img/*; do in="/media/ecbsu/rootfs/home/pi/data/img/$(basename "$date")"
+  mkdir -p $outdir/$(basename "$date")
+  for site in $(find "$in" -mindepth 1 -maxdepth 1 -type d); do
+    o="$outdir/$(basename "$date")/$(basename "$site")"
+    mkdir -p $o
+    for run in "$site"/* ; do python /home/ecbsu/Desktop/seg_dir/local_segmenter_cmd.py -i $run -o $o -c & 
+    done
+  done
+done
+
+wait
+for date in /media/ecbsu/rootfs/home/pi/data/img/*; do in="/media/ecbsu/rootfs/home/pi/data/img/$(basename "$date")"
+  for site in $(find "$in" -mindepth 1 -maxdepth 1 -type d); do
+    o="$outdir/$(basename "$date")/$(basename "$site")"
+    for run in "$site"/* ; do cp "$run/metadata.json" "$o/$(basename "$run")/metadata.json"; done
+  done 
+done
+
+# Check if the segmenter completed runs
+echo "Completed runs:"
+find $outdir -maxdepth 4 -name done.txt 
+
