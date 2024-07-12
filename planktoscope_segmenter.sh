2192fec9ea93d3b1111c6f6f1677248c4d7cb52d
pyongheng <107157547+pyongheng@users.noreply.github.com>
2024-07-12 10:48:10 +0900

    Update planktoscope_segmenter.sh
    Improved annotation

--- planktoscope_segmenter.sh
+++ planktoscope_segmenter.sh
@@ -1,19 +1,23 @@
-# Change to the date of run
+# To run local_segmenter for planktoscope-acquired images, insert SD card into the Dell workstation and run directly.
+
+# This will select the date that the segmenter will run on.
 date='20240612'
 
-# to run local_segmenter for planktoscope
+# Output and input dir information
+outdir='/media/ecbsu/vEM_data/planktoscope_data'
+in="/media/ecbsu/rootfs/home/pi/data/img/$date"
+
+# Deactivating conda and activating segmenter environment
 conda deactivate
 conda deactivate
 source /home/ecbsu/Desktop/seg_dir/seg_dir/bin/activate
 
-# to run by date folders
-outdir='/media/ecbsu/vEM_data/planktoscope_data'
-
-in="/media/ecbsu/rootfs/home/pi/data/img/$date"
+# Make output folder
 mkdir $outdir/$date
 
+# Run segmenter on all sites and runs
 for site in $(find "$in" -mindepth 1 -maxdepth 1 -type d); do
   o="$outdir/$date/$(basename "$site")"
   mkdir -p $o
-  for run in "$site"/* ; do python /home/ecbsu/Desktop/seg_dir/local_segmenter_cmd.py -i $run -o $o -c & done
+  for run in "$site"/* ; do python /home/ecbsu/Desktop/seg_dir/local_segmenter_cmd.py -i $run -o $o -c & done # the -c flag here is used to remove debug files (extremely large files)
 done
